84353d8aef452b3f7f4bd3cae2039878b733908d
iridiumtao <iridiumtao@gmail.com>
2025-01-17 18:05:39 -0500

    Configure Next.js to deploy on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,28 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = "";
+let assetPrefix = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  basePath = `/${repo}`;
+  assetPrefix = `/${repo}`;
+}
+
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
+  reactStrictMode: true,
   output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+  basePath,
+  assetPrefix,
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
