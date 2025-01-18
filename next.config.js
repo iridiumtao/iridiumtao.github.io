7bb3ca71f8d7ebeb15dc4105242c62f7d7b7efe0
iridiumtao <iridiumtao@gmail.com>
2025-01-17 19:01:34 -0500

    continue fixing deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let basePath = "";
 let assetPrefix = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}`;
-}
 
 const nextConfig = {
   reactStrictMode: true,
