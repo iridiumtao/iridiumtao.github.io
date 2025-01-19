ede3ec4a179bcf97726b955b825decb8a6b36f54
iridiumtao <iridiumtao@gmail.com>
2025-01-18 20:58:13 -0500

    ？？？ test

--- components/ContentSection/index.js
+++ components/ContentSection/index.js
@@ -25,9 +25,9 @@ const CodeBlock = {
 
 const ContentSection = ({ content }) => {
   return (
-      <div className="markdown-class">
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
+    <ReactMarkdown components={CodeBlock} className="markdown-class">
+      {content}
+    </ReactMarkdown>
   );
 };
 
